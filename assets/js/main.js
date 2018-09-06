fc67e9ad693e177021c7540e83e1b0e24069f93a
yruefenacht <yannick.ruefenacht@hotmail.com>
2018-09-06 20:42:51 +0200

    Added text and design update

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,9 @@ class Eventhandler {
 
         this.view                       = $(".view")
         this.viewSidePanel              = $(".view__sidePanel")
-        this.viewSectorTitle            = $(".view__sector__title")
-        this.viewSectorDescription      = $(".view__sector__description")
+        this.viewSectorImage            = $(".view__sector__image")
+        this.viewSectorTitle            = $(".view__sector__text__title")
+        this.viewSectorDescription      = $(".view__sector__text__description")
         this.panel                      = $(".panel")
         this.panelSector                = $(".panel__sector")
         this.panelSectorFrame           = $(".panel__sector--frame")
@@ -54,13 +55,15 @@ class Eventhandler {
 
 
         self.panelSector.click(function() {
-            self.panel.css("transform", "translateX(-60%)")
+            self.panel.css("transform", "translateX(-55%)")
             self.view.css("transform", "translateX(0)")
             self.viewSidePanel.css("opacity", "1")
 
             var sectorTitle         = $(this).find(".panel__sector__title").text()
             var sectorDescription   = $(this).find(".panel__sector__description").text()
+            var sectorImage         = $(this).css("background-image")
 
+            self.viewSectorImage.css("background-image", sectorImage)            
             self.viewSectorTitle.text(sectorTitle)
             self.viewSectorDescription.text(sectorDescription)
         })
